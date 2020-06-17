4f3f12c3bae84b59a0ea93c1579aaa4c7ad02e0f
stefaleon <stefaleon@gmail.com>
2020-06-17 15:51:01 +0300

    17 Mapping Methods & Parameters

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,6 +3,17 @@ class Pages
 {
     public function __construct()
     {
-        echo 'Pages loaded';
+
     }
+
+    public function index()
+    {
+
+    }
+
+    public function about($id)
+    {
+        echo $id;
+    }
+
 }
