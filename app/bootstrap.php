31aa282d917d2a96c80aa4957ee9c1b87d042816
stefaleon <stefaleon@gmail.com>
2020-06-19 15:21:39 +0300

    32 User Registration

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,9 @@
 // Load Config
 require_once 'config/config.php';
 
+// Load Helpers
+require_once 'helpers/url_helper.php';
+
 // Load Libraries
 // COMMENTED OUT SINCE IT IS NOW TAKEN CARE BY THE AUTOLOADER BELOW
 // require_once 'libraries/Core.php';
