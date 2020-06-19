6ed72faf0d666d79663878e6161c03643f523c78
stefaleon <stefaleon@gmail.com>
2020-06-19 14:51:19 +0300

    31 User Model & Email Check

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -3,7 +3,7 @@ class Users extends Controller
 {
     public function __construct()
     {
-
+        $this->userModel = $this->model('User');
     }
 
     public function register()
@@ -30,6 +30,11 @@ class Users extends Controller
             // Validate Email
             if (empty($data['email'])) {
                 $data['email_err'] = 'Please enter email';
+            } else {
+                // Check email
+                if ($this->userModel->findUserByEmail($data['email'])) {
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
 
             // Validate Name
