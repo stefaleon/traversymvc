6415577a42adc244052d91996e2b5c27801d037e
stefaleon <stefaleon@gmail.com>
2020-06-21 02:47:59 +0300

    43 Deleting Posts

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-  <div class='container'>
+  <div class="container">
       <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -16,7 +16,10 @@
         </ul>
 
         <ul class="navbar-nav ml-auto">
-        <?php if (isset($_SESSION['user_id'])): ?>
+          <?php if (isset($_SESSION['user_id'])): ?>
+          <li class="nav-item">
+              <a class="nav-link" href="#">Welcome <?php echo $_SESSION['user_name']; ?></a>
+            </li>
           <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
             </li>
@@ -30,5 +33,5 @@
           <?php endif;?>
         </ul>
       </div>
-  </div>
-</nav>
+    </div>
+  </nav>
